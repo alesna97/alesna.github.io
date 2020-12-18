c1c3028eb9ac8140fda68f229546a55c11e4eb55
anjas lesmana <alesna97@gmail.com>
2020-12-18 23:56:12 +0700

    add-portofolio

--- src/containers/Pages/Portofolio/index.js
+++ src/containers/Pages/Portofolio/index.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Typography } from '@material-ui/core';
+import { Box, Link, Typography } from '@material-ui/core';
 import Content from '../../../components/Content';
 
 const Portofolio = (props) => (
   <>
     <Content>
-      <Typography color="secondary">
-        Portofolio
-      </Typography>
+      <Box
+        display="flex"
+        width="100%"
+        height="100%"
+        justifyContent="center"
+        alignItems="center"
+        flexDirection="column"
+      >
+        <Typography
+          color="secondary"
+          variant="h6"
+        >
+          Public Portofolio :
+        </Typography>
+      <Link href="https://ulas.id">Ulas.id</Link>
+      <Link href="https://sembilan-tani.web.app/">Sembilan Tani</Link>
+      </Box>
     </Content>
   </>
 );
